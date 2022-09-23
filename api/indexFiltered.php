ecccf71be2e497867fd9b11a88d7d046a6cb4ea9
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2022-09-23 16:50:34 +0200

    add bonus

---
+++ api/indexFiltered.php
@@ -0,0 +1,20 @@
+<?php
+include __DIR__ . '/albums.php';
+
+$genre = isset($_GET['genre']) ? $_GET['genre'] : null;
+
+if ($genre) {
+    // soluzione 3: use
+    $albums = array_filter($albums, function ($album) use ($genre) {
+        return $genre == $album['genre'];
+    });
+}
+
+$data = [
+    'success' => true,
+    'response' => array_values($albums)
+];
+
+header('Content-Type: application/json');
+
+echo json_encode($data);
--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const app = new Vue({
     el: '#app',
     data: {
-        albums: []
+        albums: [],
+        genre: '',
+        defGen: 'Genere: ',
+        openGen: false,
+        focusGen: false,
+        ogAlbums: []
+    },
+    computed: {
+        getGenres() {
+            const genres = new Array();
+            this.ogAlbums.forEach(element => {
+                if (!genres.includes(element.genre)) genres.push(element.genre)
+            });
+
+            return genres;
+        }
     },
     methods: {
         fetchAlbums() {
@@ -10,8 +25,27 @@ const app = new Vue({
                     // console.log(res.data);
                     const { response } = res.data;
                     this.albums = response;
+                    this.ogAlbums = response;
                     console.log(this.albums);
                 })
+        },
+        fetchAlbumsFiltered() {
+            axios.get('http://localhost/esercitazioni/php-ajax-dischi/api/indexFiltered.php', {
+                params: {
+                    genre: this.genre,
+                }
+            })
+                .then(res => {
+                    // console.log(res);
+                    const { response } = res.data;
+                    this.albums = response;
+                })
+        },
+        clickHandlerGen() {
+            if (!this.focusGen) {
+                this.openGen = !this.openGen;
+            }
+            else this.focusGen = false;
         },
     },
     created() {
